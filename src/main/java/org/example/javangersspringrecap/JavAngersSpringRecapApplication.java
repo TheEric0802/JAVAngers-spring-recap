ce45199669d8240b77ca633466bfa537abf8c62f
Eric Schmidt <eric0802@web.de>
2025-04-23 09:59:53 +0200

    Initial commit

---
+++ src/main/java/org/example/javangersspringrecap/JavAngersSpringRecapApplication.java
@@ -0,0 +1,13 @@
+package org.example.javangersspringrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAngersSpringRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavAngersSpringRecapApplication.class, args);
+    }
+
+}
